78b312ecbaa0409b68af12c85bf1fb783c5288d8
adamjcoates <ajcoates0@gmail.com>
2023-01-22 18:20:46 -0700

    Added form validation

--- src/components/forms/login-form.js
+++ src/components/forms/login-form.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { icons } from '../../utils/ui-constants';
 import * as EmailValidator from 'email-validator';
 
+
 const LoginForm = () => {
   const [formData, setData] = React.useState({});
   const [errors, setErrors] = React.useState({});
@@ -39,28 +40,28 @@ const LoginForm = () => {
   };
 
   return (
-    <Square width='80%' bg='white' rounded='md' alignItems='center'>
+    <Square width='80%' height='50%' bg='white' rounded='md' alignItems='center'>
       <VStack width='80%' alignItems='center'>
-      <FormControl isRequired isInvalid={'email' in errors}>
-        <Input InputLeftElement={<Icon as={<FontAwesomeIcon icon={icons.envelope}/>}/>} placeholder='Email' onChangeText={value => setData({ ...formData, email: value})} />
-        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} _text={{ fontSize: 'xs' }} >
-          {errors.email}
-        </FormControl.ErrorMessage>
-      </FormControl>
-      <FormControl isRequired isInvalid={'password' in errors}>
-      <Input 
-       type={show ? "text" : "password"}
-       InputLeftElement={<Icon as={<FontAwesomeIcon icon={icons.user}/>}/>} 
-       InputRightElement={<Pressable onPress={() => setShow(!show)}><Icon as={<FontAwesomeIcon icon={show ? icons.eye : icons.eyeSlash}/>}/></Pressable>} 
-       placeholder='Password' onChangeText={value => setData({ ...formData, password: value})} />
-        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} _text={{ fontSize: 'xs' }} >
-          {errors.password}
-        </FormControl.ErrorMessage>
-      </FormControl>
-      <Button onPress={onSubmit}>
-        Login
-      </Button>
-    </VStack>
+        <FormControl isRequired isInvalid={'email' in errors}>
+          <Input InputLeftElement={<Icon as={<FontAwesomeIcon icon={icons.envelope}/>}/>} placeholder='Email' onChangeText={value => setData({ ...formData, email: value})} />
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} _text={{ fontSize: 'xs' }} >
+            {errors.email}
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <FormControl isRequired isInvalid={'password' in errors}>
+        <Input 
+        type={show ? "text" : "password"}
+        InputLeftElement={<Icon as={<FontAwesomeIcon icon={icons.user}/>}/>} 
+        InputRightElement={<Pressable onPress={() => setShow(!show)}><Icon as={<FontAwesomeIcon icon={show ? icons.eye : icons.eyeSlash}/>}/></Pressable>} 
+        placeholder='Password' onChangeText={value => setData({ ...formData, password: value})} />
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} _text={{ fontSize: 'xs' }} >
+            {errors.password}
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <Button onPress={onSubmit}>
+          Login
+        </Button>
+      </VStack>
     </Square>
     
   );
