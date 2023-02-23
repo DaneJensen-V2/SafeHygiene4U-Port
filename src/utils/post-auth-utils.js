42e548099e17128b2a69566d1c08eac0366f1770
adamjcoates <ajcoates0@gmail.com>
2023-02-22 17:19:39 -0700

    chore: fixed require cycle bug

--- src/utils/post-auth-utils.js
+++ src/utils/post-auth-utils.js
@@ -1,67 +1,64 @@
 import * as SecureStore from 'expo-secure-store';
-import { 
-    API_CONSTANTS, 
-    ERROR_TYPES,
-    REPURPOST_BACKEND_CONSTANTS,
-    SECURE_STORE_KEYS,
- } from './constants';
 import {
-    makeAuthenticatedRequest,
-} from './request-authenticated';
+  API_CONSTANTS,
+  ERROR_TYPES,
+  REPURPOST_BACKEND_CONSTANTS,
+  SECURE_STORE_KEYS,
+} from './constants';
 
 /**
- * Function to get the access and refresh tokens from the User's secure storage, if they exist. 
+ * Function to get the access and refresh tokens from the User's secure storage, if they exist.
  * @param keysToRetrieve - Array of keys (string constants) to retrieve)
  */
 const getSecureKeys = async (keysToRetrieve) => {
-    let retrievedKeyValues = {};
-    for (const key of keysToRetrieve) {
-        console.log(`Getting secure value for key ${key}`);
-        let value = await SecureStore.getItemAsync(key);
-        console.log(`Found value ${value} for key ${key}`);
-        retrievedKeyValues[key] = value;
-    }
-    console.log(`Got secure key-values: ${JSON.stringify(retrievedKeyValues)}`)
-    return retrievedKeyValues;
+  let retrievedKeyValues = {};
+  for (const key of keysToRetrieve) {
+    console.log(`Getting secure value for key ${key}`);
+    let value = await SecureStore.getItemAsync(key);
+    console.log(`Found value ${value} for key ${key}`);
+    retrievedKeyValues[key] = value;
+  }
+  console.log(`Got secure key-values: ${JSON.stringify(retrievedKeyValues)}`);
+  return retrievedKeyValues;
 };
 
 /**
  * Function to get a refreshed access token from Repurpost.
  */
 const getRefreshedToken = async (accessToken, refreshToken) => {
-    return fetch(REPURPOST_LOGIN, {
+  return fetch(REPURPOST_LOGIN, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(refreshToken)
-  }).then(res => res.json())
-}
-
-/**
- * Function to save the authentication tokens that were received from Repurpost to the 
- * secure store
- * 
- */
-const saveAuthTokensToSecureStore = async (accessToken, refreshToken) => {
-    console.log('Attempting to save authentication tokens to Expo secure store.');
-    console.log(`accessToken: ${accessToken}, \n\nrefreshToken: ${refreshToken}`);
-    await SecureStore.setItemAsync(SECURE_STORE_KEYS.ACCESS_TOKEN, accessToken);
-    await SecureStore.setItemAsync(SECURE_STORE_KEYS.REFRESH_TOKEN, refreshToken);
+    body: JSON.stringify(refreshToken),
+  }).then((res) => res.json());
 };
 
 /**
- * Function to delete tokens from Secure Store when the user logs out. 
+ * Function to save the authentication tokens that were received from Repurpost to the
+ * secure store
+ *
+ */
+const saveAuthTokensToSecureStore = async (accessToken, refreshToken) => {
+  console.log('Attempting to save authentication tokens to Expo secure store.');
+  console.log(`accessToken: ${accessToken}, \n\nrefreshToken: ${refreshToken}`);
+  await SecureStore.setItemAsync(SECURE_STORE_KEYS.ACCESS_TOKEN, accessToken);
+  await SecureStore.setItemAsync(SECURE_STORE_KEYS.REFRESH_TOKEN, refreshToken);
+};
+
+/**
+ * Function to delete tokens from Secure Store when the user logs out.
  */
 const removeAuthTokensFromSecureStore = async () => {
-    await SecureStore.setItemAsync(SECURE_STORE_KEYS.ACCESS_TOKEN, '');
-    await SecureStore.setItemAsync(SECURE_STORE_KEYS.REFRESH_TOKEN, '');
-    console.log("Secure Store cleared");
-}
+  await SecureStore.setItemAsync(SECURE_STORE_KEYS.ACCESS_TOKEN, '');
+  await SecureStore.setItemAsync(SECURE_STORE_KEYS.REFRESH_TOKEN, '');
+  console.log('Secure Store cleared');
+};
 
 module.exports = {
-    getSecureKeys,
-    getRefreshedToken,
-    saveAuthTokensToSecureStore,
-    removeAuthTokensFromSecureStore,
+  getSecureKeys,
+  getRefreshedToken,
+  saveAuthTokensToSecureStore,
+  removeAuthTokensFromSecureStore,
 };
